cabb4b595d0a128ed8ada593e547880dbe78b2ca
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-07 18:13:40 +0100

    promisify

---
+++ promisify.js
@@ -0,0 +1,28 @@
+
+const promisify = fn => {
+  return (...args) => new Promise(resolve => {
+    const cb = result => { resolve(result) };
+    fn.apply(this, args.concat(cb));
+  })
+}
+
+// Creating a custom setTimeout that takes callback at the end
+function customSetTimeout(...args) {
+  setTimeout(...args.reverse());
+}
+// Promisfy in action - using customSetTimeout
+let setTimeOutPromise = promisify(customSetTimeout);
+
+function printAndReturnPromise() {
+  console.log('First Callback function called');
+  return setTimeOutPromise(1000);
+}
+
+function printAfterGivenTime() {
+  console.log('Second Callback function called');
+}
+
+// Promise chain
+setTimeOutPromise(2000)
+  .then(printAndReturnPromise)
+  .then(printAfterGivenTime);
