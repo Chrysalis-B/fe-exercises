0aff291d106e141fc7026a6c62236b7a884ccf7c
Heini Ahokas <heini.ahokas@klab-berlin.com>
2023-02-07 16:35:15 +0100

    bind

---
+++ bind.js
@@ -0,0 +1,18 @@
+Function.prototype.bind = function (context) {
+  const fn = this;
+  return function () { fn.call(context) };
+}
+
+function bind(fn, context) {
+  return function () {  
+    fn.call(context);
+  }
+}
+
+const foo = function () {
+  console.log(this.bar);
+}
+
+let baz = foo.bind({ bar: 'hello' });
+
+baz();
